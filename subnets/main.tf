5b8a198403afffe53dbcdffe00209de893e9adbf
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-01-28 01:18:23 +0530

    added tf file

--- subnets/main.tf
+++ subnets/main.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "main" {
   count = length(var.subnets)
   vpc_id     = var.vpc_id
   cidr_block = var.subnets[count.index]
-  availability_zone_id = var.AZ[count.index]
+  availability_zone = var.AZ[count.index]
 
   tags = {
     Name = "Roboshop-${var.name}-snet"
