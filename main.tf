7c2d1f14f8a60212ab91f81b8546720f7634f5e5
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-01-28 00:50:04 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block           = var.cidr_block
+
+  tags = {
+    Name = "Roboshop-${var.env}-vpc"
+  }
 }
--- vars.tf
+++ vars.tf
@@ -1,1 +1,5 @@
 variable "cidr_block" {}
+
+variable "subnets" {}
+
+variable "env" {}
