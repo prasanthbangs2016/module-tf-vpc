f134e5b2fb849d90000fd80a735e6d931bff268f
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-01-28 12:18:16 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -114,4 +114,8 @@ resource "aws_vpc_peering_connection" "foo" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.default_vpc_id
   auto_accept = true
+
+  tags = {
+    Name = "Roboshop-${var.env}-to-default-aws-vpc"
+  }
 }
