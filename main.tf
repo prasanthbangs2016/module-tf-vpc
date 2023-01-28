dd529daf96b41c22e71c46c00bc40fec4fff9402
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-01-28 22:59:03 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -32,93 +32,93 @@ resource "aws_internet_gateway" "igw" {
 }
 
 
-resource "aws_route_table" "route-tables" {
-  for_each = var.subnets
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "Roboshop-Dev-${each.value["name"]}-rt"
-
-  }
-}
-#way to create route table in general, it create multiple rt for all 3
+#resource "aws_route_table" "route-tables" {
+#  for_each = var.subnets
+#  vpc_id = aws_vpc.main.id
+#
+#  tags = {
+#    Name = "Roboshop-Dev-${each.value["name"]}-rt"
+#
+#  }
+#}
+##way to create route table in general, it create multiple rt for all 3
+##resource "aws_route" "public" {
+##  route_table_id = aws_route_table.route-tables["public"].id
+##  destination_cidr_block = "0.0.0.0/0"
+##  gateway_id = aws_internet_gateway.igw.id
+##}
+##
+##output "out" {
+##  value = aws_route_table.route-tables
+##  value = aws_route_table.route-tables["public"].id
+##}
+#
 #resource "aws_route" "public" {
 #  route_table_id = aws_route_table.route-tables["public"].id
 #  destination_cidr_block = "0.0.0.0/0"
 #  gateway_id = aws_internet_gateway.igw.id
 #}
-#
 #output "out" {
-#  value = aws_route_table.route-tables
-#  value = aws_route_table.route-tables["public"].id
+#  value = module.subnets["public"].out[*].id
 #}
-
-resource "aws_route" "public" {
-  route_table_id = aws_route_table.route-tables["public"].id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
-}
-output "out" {
-  value = module.subnets["public"].out[*].id
-}
-
-resource "aws_route_table_association" "public" {
-  count = length(module.subnets["public"].out[*].id)
-  subnet_id      = element(module.subnets["public"].out[*].id, count.index )
-  route_table_id = aws_route_table.route-tables["public"].id
-}
-resource "aws_route" "private-apps" {
-  route_table_id = aws_route_table.route-tables["apps"].id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.ngw.id
-}
-
-resource "aws_route" "private-db" {
-  route_table_id = aws_route_table.route-tables["db"].id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.ngw.id
-}
-
-
-resource "aws_route_table_association" "apps" {
-  count = length(module.subnets["apps"].out[*].id)
-  subnet_id      = element(module.subnets["apps"].out[*].id, count.index )
-  route_table_id = aws_route_table.route-tables["apps"].id
-}
-
-resource "aws_route_table_association" "db" {
-  count = length(module.subnets["db"].out[*].id)
-  subnet_id      = element(module.subnets["db"].out[*].id, count.index )
-  route_table_id = aws_route_table.route-tables["db"].id
-}
-
-
-
-#eip is needed for nat gateway
-resource "aws_eip" "ngw" {
-  vpc      = true
-}
-
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.ngw.id
-  subnet_id     = module.subnets["public"].out[0].id
-
-  tags = {
-    Name = "gw NAT"
-  }
-
-
-}
-
-resource "aws_vpc_peering_connection" "peering-to-default-vpc" {
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.default_vpc_id
-  auto_accept = true
-
-  tags = {
-    Name = "Roboshop-${var.env}-to-default-aws-vpc"
-  }
-}
+#
+#resource "aws_route_table_association" "public" {
+#  count = length(module.subnets["public"].out[*].id)
+#  subnet_id      = element(module.subnets["public"].out[*].id, count.index )
+#  route_table_id = aws_route_table.route-tables["public"].id
+#}
+#resource "aws_route" "private-apps" {
+#  route_table_id = aws_route_table.route-tables["apps"].id
+#  destination_cidr_block = "0.0.0.0/0"
+#  nat_gateway_id = aws_nat_gateway.ngw.id
+#}
+#
+#resource "aws_route" "private-db" {
+#  route_table_id = aws_route_table.route-tables["db"].id
+#  destination_cidr_block = "0.0.0.0/0"
+#  nat_gateway_id = aws_nat_gateway.ngw.id
+#}
+#
+#
+#resource "aws_route_table_association" "apps" {
+#  count = length(module.subnets["apps"].out[*].id)
+#  subnet_id      = element(module.subnets["apps"].out[*].id, count.index )
+#  route_table_id = aws_route_table.route-tables["apps"].id
+#}
+#
+#resource "aws_route_table_association" "db" {
+#  count = length(module.subnets["db"].out[*].id)
+#  subnet_id      = element(module.subnets["db"].out[*].id, count.index )
+#  route_table_id = aws_route_table.route-tables["db"].id
+#}
+#
+#
+#
+##eip is needed for nat gateway
+#resource "aws_eip" "ngw" {
+#  vpc      = true
+#}
+#
+#resource "aws_nat_gateway" "ngw" {
+#  allocation_id = aws_eip.ngw.id
+#  subnet_id     = module.subnets["public"].out[0].id
+#
+#  tags = {
+#    Name = "gw NAT"
+#  }
+#
+#
+#}
+#
+#resource "aws_vpc_peering_connection" "peering-to-default-vpc" {
+#  peer_vpc_id   = aws_vpc.main.id
+#  vpc_id        = var.default_vpc_id
+#  auto_accept = true
+#
+#  tags = {
+#    Name = "Roboshop-${var.env}-to-default-aws-vpc"
+#  }
+#}
 
 #resource "aws_route" "peering" {
 #  count   = length(aws_route_table.route-tables[*].id)
