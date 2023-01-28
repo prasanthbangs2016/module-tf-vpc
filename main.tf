64867c6041b140c655c3f364f2393373d951581f
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-01-28 12:23:36 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ resource "aws_nat_gateway" "ngw" {
 
 }
 
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "peering-to-default-vpc" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.default_vpc_id
   auto_accept = true
@@ -118,4 +118,12 @@ resource "aws_vpc_peering_connection" "foo" {
   tags = {
     Name = "Roboshop-${var.env}-to-default-aws-vpc"
   }
+}
+
+resource "aws_route" "peering" {
+  for_each = var.subnets
+  route_table_id = aws_route_table.route-tables[each.value[name]].id
+  destination_cidr_block = "0.0.0.0/0"
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering-to-default-vpc.id
+
 }
