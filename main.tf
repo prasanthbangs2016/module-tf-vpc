f02df8a7ac34837376ce4c4a78fc733b55dc429b
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-01-28 08:49:38 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -11,17 +11,16 @@ module "subnets" {
   source   = "./subnets"
   name     = each.value["name"]
   subnets  = each.value["subnet_cidr"]
-  vpc_id   = aws_vpc.main.id
+  vpc_id   = var.vpc_id
   AZ       = var.AZ
-  env      = ""
-  igw      = ""
-  ngw      = try(each.value["ngw"], false]
-  igw      = try(each.value["igw"], false]
+  env      = "var.env"
+  ngw      = try(each.value["ngw"], false)
+  igw      = try(each.value["igw"], false)
   env      = var.env
 
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
         vpc_id = var.vpc_id
 
         tags = {
