4d3cf0b00497eb86f90f4aabe9e1054bf3cd36d8
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-01-30 17:23:55 +0530

    added tf file

--- routes/main.tf
+++ routes/main.tf
@@ -7,17 +7,17 @@ resource "aws_route_table" "route-tables" {
   }
 }
 
-resource "aws_route_table_association" "assoc" {
-  count = length(var.subnet_ids[var.name].out[*].id)
-  subnet_id      = element(var.subnet_ids[var.name].out[*].id, count.index )
-  route_table_id = aws_route_table.route-tables.id
+#resource "aws_route_table_association" "assoc" {
+#  count = length(var.subnet_ids[var.name].out[*].id)
+#  subnet_id      = element(var.subnet_ids[var.name].out[*].id, count.index )
+#  route_table_id = aws_route_table.route-tables.id
 }
 
-#resource "aws_route_table_association" "assoc" {
-#  count = length(var.subnet_ids[var.name].subnet_ids)
-#  subnet_id      = element(var.subnet_ids[var.name].subnet_ids, count.index )
-#  route_table_id = aws_route_table.route-tables.id
-#}
+resource "aws_route_table_association" "assoc" {
+  count = length(var.subnet_ids[var.name].subnet_ids)
+  subnet_id      = element(var.subnet_ids[var.name].subnet_ids, count.index)
+  route_table_id = aws_route_table.route-tables.id
+}
 
 
 
