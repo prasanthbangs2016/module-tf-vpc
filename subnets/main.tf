05536ef2bbead9b07c6d8ae609ff8387b104a8e3
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-01-30 17:28:28 +0530

    added tf file

--- subnets/main.tf
+++ subnets/main.tf
@@ -12,3 +12,11 @@ resource "aws_subnet" "main" {
 output "out" {
   value = aws_subnet.main
 }
+
+output "subnets" {
+  value = aws_subnet.main
+}
+
+output "subnet_ids" {
+  value = [for s in aws_subnet.main : s.id]
+}
