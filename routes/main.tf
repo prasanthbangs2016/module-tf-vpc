a4e47e7bf6007115900249c56c5345538316f7e5
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-01-30 17:24:27 +0530

    added tf file

--- routes/main.tf
+++ routes/main.tf
@@ -11,7 +11,7 @@ resource "aws_route_table" "route-tables" {
 #  count = length(var.subnet_ids[var.name].out[*].id)
 #  subnet_id      = element(var.subnet_ids[var.name].out[*].id, count.index )
 #  route_table_id = aws_route_table.route-tables.id
-}
+#}
 
 resource "aws_route_table_association" "assoc" {
   count = length(var.subnet_ids[var.name].subnet_ids)
