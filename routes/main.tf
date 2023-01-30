8eaeb47f5484465ff259b098a0c900540d2e115b
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-01-30 17:15:05 +0530

    added tf file

--- routes/main.tf
+++ routes/main.tf
@@ -7,22 +7,30 @@ resource "aws_route_table" "route-tables" {
   }
 }
 
+#resource "aws_route_table_association" "assoc" {
+#  count = length(var.subnet_ids[var.name].out[*].id)
+#  subnet_id      = element(var.subnet_ids[var.name].out[*].id, count.index )
+#  route_table_id = aws_route_table.route-tables.id
+#}
+
 resource "aws_route_table_association" "assoc" {
-  count = length(var.subnet_ids[var.name].out[*].id)
-  subnet_id      = element(var.subnet_ids[var.name].out[*].id, count.index )
+  count = length(var.subnet_ids[var.name].subnet_ids)
+  subnet_id      = element(var.subnet_ids[var.name].subnet_ids, count.index )
   route_table_id = aws_route_table.route-tables.id
 }
 
 
+
+
 resource "aws_route" "public" {
-  count = var.name == var.igw ? 1 :0
+  count = var.name == var.igw ? 1 : 0
   route_table_id = aws_route_table.route-tables.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = var.gateway_id
 }
 
 resource "aws_route" "private" {
-  count = var.name == var.ngw ? 1 :0
+  count = var.name == var.ngw ? 1 : 0
   route_table_id = aws_route_table.route-tables.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = var.nat_gateway_id
