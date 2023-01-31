edb02b5a5fe4e263da3e0432c1d87a556170bb88
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-01-31 18:55:41 +0530

    added tf file

--- routes/main.tf
+++ routes/main.tf
@@ -29,13 +29,12 @@ resource "aws_route" "public" {
   gateway_id             = var.gateway_id
 }
 #
-#resource "aws_route" "private" {
-#  count = var.name == var.ngw ? 1 : 0
-#  #count = var.name == "private" ? 1 : 0
-#  route_table_id = aws_route_table.route-tables.id
-#  destination_cidr_block = "0.0.0.0/0"
-#  nat_gateway_id = var.nat_gateway_id
-#}
+resource "aws_route" "private" {
+  count                  = var.ngw ? 1 : 0
+  route_table_id         = aws_route_table.route-tables.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = var.nat_gateway_id
+}
 
 
 
