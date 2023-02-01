724b94b81de47d1864963d5eb65bae71058750d3
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-01 11:47:34 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -138,3 +138,10 @@ resource "aws_vpc_peering_connection" "peering-to-default-vpc" {
     Name = "Roboshop-${var.env}-to-default-aws-vpc"
   }
 }
+
+
+resource "aws_route" "peering-route-on-default-route-table" {
+  route_table_id = var.default_vpc_cidr
+  destination_cidr_block = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering-to-default-vpc.id
+}
